a4f9205cdbaf69ed2aa2d60433b00829dd9056f9
larrras <larasatiimania@gmail.com>
2023-06-06 00:05:15 +0700

    Laras Update

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ app=Flask(__name__)
 def home():
     if request.method=='POST':
         # Handle POST Request here
-        return render_template('index.html')
-    return render_template('index.html')
+        return render_template('home.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
