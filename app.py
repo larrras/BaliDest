8653753894c38862f0d0c7e8aa6767be80fb8e76
R A N D Y <randhyar955@gmail.com>
2023-06-08 19:36:12 +0800

    ignore dan akan merubah beberapa server login

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
-from flask import Flask,redirect,url_for,render_template,request
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
 
-from flask import Flask, render_template, request, jsonify
+# import os
+# from os.path import join, dirname
+# from dotenv import load_dotenv
+
+from flask import Flask, render_template, request, jsonify, redirect
 from pymongo import MongoClient
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+# dotenv_path = join(dirname(__file__), '.env')
+# load_dotenv(dotenv_path)
 
-MONGODB_URI = os.environ.get("MONGODB_URI")
-DB_NAME =  os.environ.get("DB_NAME")
+# MONGODB_URI = os.environ.get("MONGODB_URI")
+# DB_NAME =  os.environ.get("DB_NAME")
 
-client = MongoClient(MONGODB_URI)
+# client = MongoClient(MONGODB_URI)
 
-db = client[DB_NAME]
+# db = client[DB_NAME]
 
 app=Flask(__name__)
 
